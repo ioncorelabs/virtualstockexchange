fc596bce7aac239189ed6500d981b0cae4067a95
morales.john <morales.john@cbee4c46-9f34-11dd-9348-5985383acdef>
2008-10-21 06:06:35 +0000

    * initial commit based on NewsApp.rar emailed to everyone on 10/18/2008.

---
+++ trunk/NewsApp/NewsApp-ejb/src/java/ejb/LoginEntityFacadeLocal.java
@@ -0,0 +1,31 @@
+/*
+ * LoginEntityFacadeLocal.java
+ *
+ * Created on October 18, 2008, 5:24 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package ejb;
+
+import java.util.List;
+import javax.ejb.Local;
+
+/**
+ *
+ * @author Vaibhav
+ */
+@Local
+public interface LoginEntityFacadeLocal {
+    void create(LoginEntity loginEntity);
+
+    void edit(LoginEntity loginEntity);
+
+    void destroy(LoginEntity loginEntity);
+
+    LoginEntity find(Object pk);
+
+    List findAll(String userid, String pwd);
+    
+}
