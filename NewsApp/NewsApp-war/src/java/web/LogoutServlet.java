849e311d3780d634bab41e442cb554a29860955b
milind.nimesh <milind.nimesh@cbee4c46-9f34-11dd-9348-5985383acdef>
2008-11-01 17:29:11 +0000

    * adding logout servlet

---
+++ NewsApp/NewsApp-war/src/java/web/LogoutServlet.java
@@ -0,0 +1,94 @@
+/*
+ * LogoutServlet.java
+ *
+ * Created on November 1, 2008, 12:29 PM
+ */
+
+package web;
+
+import java.io.*;
+import java.net.*;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+/**
+ *
+ * @author Milind Nimesh
+ * @version
+ */
+public class LogoutServlet extends HttpServlet {
+    
+    /** Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+        HttpSession session = request.getSession(true);
+        session.invalidate();
+
+        
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Virtual Stock Exchance: Logged out</title>");
+        out.println("</head>");
+        out.println("<body>");
+        
+                //Common Styling Code
+        out.println("<link href=\"greeny.css\" rel=\"stylesheet\" type=\"text/css\" />");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<div id=\"tot\">");
+        out.println("<div id=\"header\">");
+        out.println("<img src=\"img/genericlogo.png\" align=\"left\" alt=\"company logo\"/> <span class=\"title\">Virtual Stock Exchange</span>");
+        out.println("<div class=\"slogan\">Bulls & Bears</div>");       
+        out.println("<div id=\"corp\">");
+        out.println("<div class=\"main-text\">");
+        //Common Ends
+        
+        out.println("<span class=\"ttitle\" style=\"580px;\">You have been Logged Out</span><br>");
+        
+        out.println("<span class=\"ttitle\" style=\"580px;\"><a href='AdminServlet'>Click Here to Login again</a></span><br>");
+       
+                              //Common Starts
+        out.println("</div></div>");
+        out.println("<div class=\"clear\"></div>");        
+        out.println("<div class=\"footer\"><span style=\"margin-left:400px;\">The Bulls & Bears Team</span></div>");
+        out.println("</div>");
+        //Common Ends
+        out.println("</body>");
+        out.println("</html>");
+        
+        out.close();
+    }
+    
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    
+    /** Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    
+    /** Returns a short description of the servlet.
+     */
+    public String getServletInfo() {
+        return "Short description";
+    }
+    // </editor-fold>
+}
